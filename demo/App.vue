699fa7932460926426da4f80d88c59501137452a
Byron <byronogis@outlook.com>
2023-06-26 17:58:22 +0800

    chore: update demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,11 +1,28 @@
+<script setup lang="ts">
+const list = [1, 2, 3, 4, 5, 6]
+</script>
+
 <template>
   <div class="demo">
-    <MyComponent />
+    <MyComponent :list="list">
+      <template #default="{ item }">
+        <div class="item">
+          {{ item }}
+        </div>
+      </template>
+    </MyComponent>
   </div>
 </template>
 
 <style>
 .demo {
   text-align: center;
+}
+
+.item {
+  width: 300px;
+  height: 50px;
+  background-color: #ccc;
+  margin-right: 10px;
 }
 </style>
